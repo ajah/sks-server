4a966988f210c224e22859479c50a3dff3702827
brittwitham <brittany.witham@gmail.com>
2022-11-28 19:00:56 -0500

    Fixes formatting errors

--- controllers/elasticsearch_controller.py
+++ controllers/elasticsearch_controller.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 import pprint
 import json
 import sys
-import os 
+import os
 
 # decrease the maxInt value by factor 10 as long as the OverflowError occurs.
 maxInt = sys.maxsize
@@ -16,7 +16,7 @@ while True:
         csv.field_size_limit(maxInt)
         break
     except OverflowError:
-        maxInt = int(maxInt/10)
+        maxInt = int(maxInt / 10)
 
 pp = pprint.PrettyPrinter(indent=2)
 
@@ -25,42 +25,50 @@ load_dotenv()
 
 ents_es = "sks-backend/data/processed/ents_es_upload.csv"
 
+
 def csv_to_json(filepath):
     df = pd.read_csv(filepath)
     json_df = df.to_json(orient='records')
     return json.loads(json_df)
 
-def connect_elasticsearch():
-  # es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-  es_username = os.getenv('ES_USERNAME')
-  es_password = os.getenv('ES_PASSWORD')
-  es = Elasticsearch(
-    cloud_id="SKS_Project:bm9ydGhhbWVyaWNhLW5vcnRoZWFzdDEuZ2NwLmVsYXN0aWMtY2xvdWQuY29tJGNlZGUyYjk4M2ZmZjRlYWI5ZDZkNzMxZmM3Nzc1YzU4JGU2ZDA1OTFmYmViZjRjY2Y4YWYxZDJlNGE5MzhiZmEx",
-    http_auth=(es_username, es_password),
-  )
-  
-  if es.ping():
-    print('Connected to ES')
-  else:
-    print('ES could not connect')
 
-  return es
+def connect_elasticsearch():
+    # es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    es_username = os.getenv('ES_USERNAME')
+    es_password = os.getenv('ES_PASSWORD')
+    es = Elasticsearch(
+        cloud_id="SKS_Project:bm9ydGhhbWVyaWNhLW5vcnRoZWFzdDEuZ2NwLmVsYXN0aWMtY2xvdWQuY29tJGNlZGUyYjk4M2ZmZjRlYWI5ZDZkNzMxZmM3Nzc1YzU4JGU2ZDA1OTFmYmViZjRjY2Y4YWYxZDJlNGE5MzhiZmEx",
+        http_auth=(es_username, es_password),
+    )
+
+    if es.ping():
+        print('Connected to ES')
+    else:
+        print('ES could not connect')
+
+    return es
+
 
 es = connect_elasticsearch()
 
 # Bulk upload working for entities
+
+
 def bulk_upload(doc, index, es=es):
-     with open(doc, 'r') as outfile:
+    with open(doc, 'r') as outfile:
         reader = csv.DictReader(outfile)
-        next(reader) # Skips header row
+        next(reader)  # Skips header row
         helpers.bulk(es, reader, index=index, doc_type="type")
 
 # Bulk upload not working for activities, adding line by line until resolved
+
+
 def upload_data(doc, bool, index, es=es):
-  if bool == True:
-      data = csv_to_json(doc)
-      for d in data:
-        r = store_record(es, index, d) 
+    if bool == True:
+        data = csv_to_json(doc)
+        for d in data:
+            r = store_record(es, index, d)
+
 
 def store_record(es_object, index, data):
     is_stored = True
@@ -72,22 +80,23 @@ def store_record(es_object, index, data):
         print('Error in indexing data')
         print(str(ex))
         is_stored = False
-    finally: 
-        return is_stored 
+    finally:
+        return is_stored
+
 
 def delete_index(index, es=es):
-  try:
-    es.indices.delete(index=index, ignore=[400, 404])
-  except Exception as ex:
-    print(ex)
+    try:
+        es.indices.delete(index=index, ignore=[400, 404])
+    except Exception as ex:
+        print(ex)
 
 
 def search_all_records(index, es=es):
-  query = {"query":{"match_all": {}}}
-  res = es.search(index=index, body=query)
-  pp.pprint("Total hits: {}".format(res['hits']['total']))
+    query = {"query": {"match_all": {}}}
+    res = es.search(index=index, body=query)
+    pp.pprint("Total hits: {}".format(res['hits']['total']))
 
-  return res['hits']
+    return res['hits']
 
 # TODO: Refactor count_records & search_records so query is not repeated
 
@@ -207,19 +216,18 @@ def build_query(keyword, operator, municipality, region, terms, size=None):
             }
         }
     }
-}
-  print(query)
-  index = None
-  if 'activity' in doctype and 'entity' in doctype:
-    index = "new-activities,entities" 
-    # index = "*" 
-  elif doctype == ['activity']:
-    index = 'new-activities'
-  elif doctype == ['entity']:
-    index = 'entities'
 
-    if size is not None:
-        query['size'] = size
+    index = None
+    if 'activity' in doctype and 'entity' in doctype:
+        index = "new-activities,entities"
+        # index = "*"
+    elif doctype == ['activity']:
+        index = 'new-activities'
+    elif doctype == ['entity']:
+        index = 'entities'
+
+        if size is not None:
+            query['size'] = size
 
     return query
 
@@ -252,15 +260,13 @@ def search_records(keyword, doctype, operator, size=None,
 
     res = es.search(index=index, body=query)
 
-
-  return res['hits']
+    return res['hits']
 
 
 def format_download(data):
     hits = [d['_source'] for d in data['hits']]
     csv = pd.DataFrame([x for x in hits]).drop(columns='Unnamed: 0').to_csv()
     return csv
-
 
 
 if __name__ == '__main__':
@@ -271,4 +277,3 @@ if __name__ == '__main__':
     print(json.dumps(test_query))
     # upload_data(acts_es,True, index='new-activities')
     # upload_data(ents_es,True, index='entities')
-
