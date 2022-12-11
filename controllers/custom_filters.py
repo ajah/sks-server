99632421133fcd807f84c3f16ce19bedb0865f91
brittwitham <brittany.witham@gmail.com>
2022-12-11 17:46:27 -0500

    Adds custom filters

---
+++ controllers/custom_filters.py
@@ -0,0 +1,14 @@
+CUSTOM_FILTERS = {
+    'efc_sustainability': {
+        "include": ['energy efficient', 'biodiversity', 'recyclable'],
+        "exclude": ['water polo']
+    },
+    'efc_climate%20change': {
+        "include": ['carbon footprint', 'extreme weather', 'greenhouse gases'],
+        "exclude": ['water polo']
+    },
+    'efc_climate%20education': {
+        "include": ['global warming', 'UNFCC', 'water conservation'],
+        "exclude": ['water polo']
+    }
+}
